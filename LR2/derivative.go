9658ab9e4c4eca79a43c94223c93c87d0c4e105c
Andryuha <astik452@gmail.com>
2023-04-11 23:07:29 +0300

    Cleanup

--- LR2/derivative.go
+++ LR2/derivative.go
@@ -8,32 +8,23 @@ func CalculateDerivatives() ([]float64, []float64) {
 
 	r := len(X)-2
 
-	// First derivative
+	// Calculate derivatives
     for i := 0; i < 4; i++ {
         var d1 float64
-        if i == 0 {
-            d1 = (Y[1] - Y[0]) / (X[1] - X[0])
-        } else if i == r {
-            d1 = (Y[r] - Y[r-1]) / (X[r] - X[r-1])
-        } else {
-            d1 = (Y[i+1] - Y[i-1]) / (X[i+1] - X[i-1])
-        }
-
-		dx = append(dx, d1)
-    }
-    
-    // Second derivative
-    for i := 0; i < 4; i++ {
         var d2 float64
         if i == 0 {
+            d1 = (Y[1] - Y[0]) / (X[1] - X[0])
             d2 = (Y[2] - 2*Y[1] + Y[0]) / ((X[1] - X[0])*(X[2] - X[0]))
         } else if i == r {
+            d1 = (Y[r] - Y[r-1]) / (X[r] - X[r-1])
             d2 = (Y[r] - 2*Y[r-1] + Y[r-2]) / ((X[r] - X[r-1])*(X[r] - X[r-2]))
         } else {
+            d1 = (Y[i+1] - Y[i-1]) / (X[i+1] - X[i-1])
             d2 = (Y[i+1] - 2*Y[i] + Y[i-1]) / ((X[i+1] - X[i])*(X[i] - X[i-1]))
         }
 
-		ddx = append(ddx, d2)
+		dx = append(dx, d1)
+        ddx = append(ddx, d2)
     }
 
 	return dx, ddx
